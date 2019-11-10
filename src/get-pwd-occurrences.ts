37fd3e7d671612c5689256159a1c102d517fde12
Claudio Corridore <claudio.corridore@gmail.com>
2019-11-10 21:56:46 +0000

    Fix indentation

--- src/get-pwd-occurrences.ts
+++ src/get-pwd-occurrences.ts
@@ -1,17 +1,15 @@
 function getPwdOccurrences(pwdTail: string, pwdRange: string[]): number {
   let occurrences = 0;
 
-  pwdRange.some(
-    (line): boolean => {
-      const [pwd, num] = line.split(":");
+  pwdRange.some((line): boolean => {
+    const [pwd, num] = line.split(":");
 
-      if (pwd.toUpperCase() === pwdTail.toUpperCase()) {
-        occurrences = parseInt(num, 10);
-      }
-
-      return occurrences > 0;
+    if (pwd.toUpperCase() === pwdTail.toUpperCase()) {
+      occurrences = parseInt(num, 10);
     }
-  );
+
+    return occurrences > 0;
+  });
 
   return occurrences;
 }
