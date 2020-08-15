3c8db20c8c555ef0fd3005739c61ad5d124d8922
Claudio Corridore <claudio.corridore@gmail.com>
2020-08-15 08:43:52 +0100

    Update deps (#8)
    * Update dependencies
    
    * Fix bundler issue

--- specs/encrypt-pwd.spec.ts
+++ specs/encrypt-pwd.spec.ts
@@ -3,6 +3,6 @@ import { encryptPwd } from "../src/encrypt-pwd";
 test("encryptPwd", (): void => {
   expect(encryptPwd("test")).toStrictEqual([
     "a94a8",
-    "fe5ccb19ba61c4c0873d391e987982fbbd3"
+    "fe5ccb19ba61c4c0873d391e987982fbbd3",
   ]);
 });
--- src/encrypt-pwd.ts
+++ src/encrypt-pwd.ts
@@ -1,9 +1,7 @@
 import { createHash } from "crypto";
 
 function encryptPwd(pwd: string): string[] {
-  const epwd = createHash("sha1")
-    .update(pwd)
-    .digest("hex");
+  const epwd = createHash("sha1").update(pwd).digest("hex");
 
   return [epwd.substring(0, 5), epwd.substring(5)];
 }
